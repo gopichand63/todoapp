888a425793773800c8bcdfbe9c151915d594c2fc
gopichand rudroju <rudrojugopichand@gmail.com>
2025-04-01 22:21:14 +0530

    Initial commit

---
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -0,0 +1,38 @@
+package com.app.todoapp.controller;
+
+
+import com.app.todoapp.models.Task;
+import com.app.todoapp.services.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+//@RequestMapping("/tasks")
+public class TaskController {
+
+    private TaskService taskService;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public String getTasks(Model model){
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks",tasks);
+        return "tasks";
+    }
+    @PostMapping
+    public String createTasks(String title){
+         taskService.createTasks(title);
+
+        return "redirect:/";
+    }
+
+
+
+}
